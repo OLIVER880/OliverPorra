6cee0a943fc94f662a0761940c95dc1a4b0afbc4
OLIVER880 <vitinhooliver44@gmail.com>
2021-02-03 06:05:54 -0300

    first commit

---
+++ src/Olivermenu.js
@@ -0,0 +1,40 @@
+const Olivermenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do ঔৣ͡𝙊͢𝙡𝙞𝙫𝙚͢𝙧𝙠𝙠͢𝙠:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *ঔৣ͡𝙊͢𝙡𝙞𝙫𝙚͢𝙧𝙠𝙠͢𝙠*
+  DUVIDAS? 👇
+  WA.me/5511969060017
+╚════════════════════`
+}
+
+exports.olivermenu = olivermenu
+
+
+
+
+
+
+
+
